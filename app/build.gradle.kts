7fbaf595a6942abff444745ea5b5d2f56820dc0b
linho <borsa>
2024-10-06 14:22:26 +0900

    add kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.org.jetbrains.kotlin.kapt)
 }
 
 android {
@@ -41,8 +42,9 @@ android {
 
 dependencies {
 
-
     implementation(libs.androidx.room)
+    kapt(libs.androidx.room.kapt)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
